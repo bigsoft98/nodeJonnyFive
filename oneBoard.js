94ac0d0bee74daf4c980da077d4c545978b3995e
Kevin HU <hukun98@gmail.com>
2018-05-10 22:32:37 +1000

    adding green and red led

--- oneBoard.js
+++ oneBoard.js
@@ -6,6 +6,10 @@ var board = new five.Board();;
 
 var yellowLed;
 var yellowLedCommandTopic ='iot/platform/command/yellowLed';
+var redLed;
+var redLedCommandTopic ='iot/platform/command/redLed';
+var greenLed;
+var greenLedCommandTopic ='iot/platform/command/greenLed';
 
 var tempSensor;
 var tempSensorDataPublishTopic ='iot/platform/data/temp';
@@ -13,17 +17,19 @@ var tempSensorDataPublishTopic ='iot/platform/data/temp';
 
 var mqttClient = mqtt.connect('mqtt://localhost:1883');
 
-mqttClient.subscribe(yellowLedCommandTopic); 
+mqttClient.subscribe([yellowLedCommandTopic,redLedCommandTopic,greenLedCommandTopic]); 
 
 
 board.on("ready", function() {
     
     yellowLed = new five.Led(4);
+    redLed = new five.Led(2);
+    greenLed = new five.Led(3);
     flameSensor = new five.Sensor("A2");
     tempSensor = new five.Thermometer({
         controller: "LM35",
         pin: "A0",
-        freq: 1000
+        freq: 2000
     });
     
 
@@ -40,11 +46,25 @@ board.on("ready", function() {
 
     mqttClient.on('message', function (topic, payload) {
         var message = payload.toString();
-        console.log('Incoming message['+message+']');
-        if(message === 'on'){
-            yellowLed.on();
-        }else{
-            yellowLed.off();
+        console.log('Incoming message ['+message+'] from topic ['+topic+']');
+        if(topic==yellowLedCommandTopic){
+            if(message === 'on'){
+                yellowLed.on();
+            }else{
+                yellowLed.off();
+            }
+        }else if(topic==redLedCommandTopic){
+            if(message === 'on'){
+                redLed.on();
+            }else{
+                redLed.off();
+            }
+        }else if(topic==greenLedCommandTopic){
+            if(message === 'on'){
+                greenLed.on();
+            }else{
+                greenLed.off();
+            }
         }
     });
 })
